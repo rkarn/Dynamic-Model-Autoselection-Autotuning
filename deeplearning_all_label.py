e298691bbd7f27d4c0821c3510de7c775d4c290f
Rupesh Raj Karn <rkarn@masdar.ac.ae>
2019-03-09 12:56:50 +0400

    Create deeplearning_all_label.py

---
+++ deeplearning_all_label.py
@@ -0,0 +1,126 @@
+import os
+#os.chdir('/Users/rupesh.karn/Desktop/WorkPart-1/UNSW-NB15 Dataset')
+import pandas as pd
+import numpy as np
+import csv
+import os
+import time
+from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
+import pylab
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.cluster import KMeans
+pylab.rcParams['figure.figsize'] = (16.0, 5.0)
+import sklearn
+
+# Read in the training CSV file
+print "Reading Training csv file."
+df1 = pd.read_csv("UNSW_NB15_training-set.csv")
+df1.drop('label', axis=1, inplace=True)
+
+#One hot encoding the string variables
+obj_df1=df1
+obj_df1["proto"] = obj_df1["proto"].astype('category')
+obj_df1["service"] = obj_df1["service"].astype('category')
+obj_df1["state"] = obj_df1["state"].astype('category')
+obj_df1["proto_cat"] = obj_df1["proto"].cat.codes
+obj_df1["service_cat"] = obj_df1["service"].cat.codes
+obj_df1["state_cat"] = obj_df1["state"].cat.codes
+
+obj_df1["proto"] = obj_df1["proto_cat"]
+obj_df1["service"] = obj_df1["service_cat"]
+obj_df1["state"] = obj_df1["state_cat"]
+
+obj_df1.drop('proto_cat', axis=1, inplace=True)
+obj_df1.drop('service_cat', axis=1, inplace=True)
+obj_df1.drop('state_cat', axis=1, inplace=True)
+
+Y_train_all_attacks = obj_df1["attack_cat"]
+X_train = obj_df1.values[:,:-1]
+
+#Normalizing train set
+for j in range(0,43):
+    maximum = max(X_train[:,j])
+    for i in range(0,len(X_train)):
+        X_train[i,j] = round(X_train[i,j]/maximum,3)
+
+#Make a catagorical cloumn for each type of label in trainset
+obj_df1=pd.get_dummies(obj_df1, columns=["attack_cat"])
+Y_train_each_attach = obj_df1.values[:,-10:]
+        
+# Read in the testing CSV file 
+print "Reading Testing csv file."
+df2 = pd.read_csv("UNSW_NB15_testing-set.csv")
+df2.drop('label', axis=1, inplace=True)
+
+#One hot encoding the string variables
+obj_df2=df2
+obj_df2["proto"] = obj_df2["proto"].astype('category')
+obj_df2["service"] = obj_df2["service"].astype('category')
+obj_df2["state"] = obj_df2["state"].astype('category')
+obj_df2["proto_cat"] = obj_df2["proto"].cat.codes
+obj_df2["service_cat"] = obj_df2["service"].cat.codes
+obj_df2["state_cat"] = obj_df2["state"].cat.codes
+
+obj_df2["proto"] = obj_df2["proto_cat"]
+obj_df2["service"] = obj_df2["service_cat"]
+obj_df2["state"] = obj_df2["state_cat"]
+
+obj_df2.drop('proto_cat', axis=1, inplace=True)
+obj_df2.drop('service_cat', axis=1, inplace=True)
+obj_df2.drop('state_cat', axis=1, inplace=True)
+
+Y_test_all_attacks = obj_df2["attack_cat"]
+X_test = obj_df2.values[:,:-1]
+
+#Normalizing test set
+for j in range(0,43):
+    maximum = max(X_train[:,j])
+    for i in range(0,len(X_test)):
+        X_test[i,j] = round(X_test[i,j]/maximum,3)
+        
+#Make a catagorical cloumn for each type of label in testset
+obj_df2=pd.get_dummies(obj_df2, columns=["attack_cat"])
+Y_test_each_attach = obj_df2.values[:,-10:]
+
+cleanup_nums = {"Worms":0, "Shellcode":1, "Reconnaissance":2, "Normal":3, "Generic":4, "Fuzzers":5, "Exploits":6, "DoS":7, "Backdoor":8, "Analysis":9}
+Y_train_all_attacks.replace(cleanup_nums,inplace=True)
+Y_test_all_attacks.replace(cleanup_nums,inplace=True)
+print('Trainset size: ',X_train.shape,Y_train_all_attacks.shape,'Testset size: ',X_test.shape,Y_test_all_attacks.shape)
+
+
+#Multilabel classification
+from keras.models import Sequential
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from keras.layers import Dense, Dropout, Activation
+import keras.initializers
+
+hidden_units = 100
+
+# Model for multiclass classification
+def create_baseline():
+    model = Sequential()
+    keras.initializers.he_normal(seed=None)
+    model.add(Dense(60, input_dim=43,  activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(60,  activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(10,  activation='softmax'))
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+# evaluate model with standardized dataset
+print(create_baseline().summary())
+estimator = KerasClassifier(build_fn=create_baseline, batch_size=100, verbose=0)
+kfold = StratifiedKFold(n_splits=2, shuffle=True, random_state=seed)
+results = cross_val_score(estimator, X_train, Y_train_all_attacks, cv=kfold)
+print("Train Results: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+
+results = cross_val_score(estimator, X_test, Y_test_all_attacks, cv=kfold)
+print("Test Results: %.2f%% (%.2f%%) \n" % (results.mean()*100, results.std()*100))
